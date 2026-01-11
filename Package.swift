a5f4f9f3796281ae387c6a787855aafd1b78cac9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-11 20:02:32 +0100

    Migrate to swift-primitives
    - Update imports and dependencies for primitives refactoring

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         // Add RFC dependencies here as needed
-        // .package(url: "https://github.com/swift-standards/swift-rfc-1123.git", from: "0.1.0"),
+        // .package(path: "../swift-rfc-1123.git"),
     ],
     targets: [
         .target(
